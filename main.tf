81b02eb936f7764f1a7e3012d5f6531ee6a59acf
LAD-PHP <46951395+LAD-PHP@users.noreply.github.com>
2023-08-24 16:18:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,27 +7,27 @@ provider "vcd" {
 }
 
 #SET a TEMPLATE
-#dunno why i'm defining 100500 variables or what is this shit? 
 data "vcd_catalog" "my-catalog" {
   org  = "unix_u4"
   name = "VK_S3_TEMPL"
 }
-#same as above, i define catalog, now i must define fucking template, why i can define it in 1 command?
+
 data "vcd_catalog_vapp_template" "centos7" {
   org        = "unix_u4"
   catalog_id = data.vcd_catalog.my-catalog.id
   name       = "vk-temp"
 }
 
-#variable for ssh_keys dunno how to specify this shit in another file annd iunclude to this bull shit
-variable "ssh_key_private" {
-  description = "Path to the private key file for SSH authentication"
-  type        = string
-  default     = "/root/.ssh/id_rsa"  # Replace with the actual path to your private key file
-}
+#КАКОЙ-ТО ЕБАНЫЙ НЕЗАВИСИМЫЙ ДИСК
+#resource "vcd_independent_disk" "disk" {
+#  count = 3
+#  name         = "logDisk${count.index + 1}"
+#  size_in_mb   = "10512"
+#  bus_type     = "SCSI"
+#  bus_sub_type = "VirtualSCSI"
+#}
 
 resource "vcd_vapp_vm" "TestVm" {
-  #count it's like for_each, but not =D
   count = 3  # This will create three instances
 
   vapp_name     = "vk-storage"
@@ -38,13 +38,11 @@ resource "vcd_vapp_vm" "TestVm" {
   cpu_cores     = 1
   hardware_version = "vmx-15"
 
-  #this stupidity shit must be specifed otherwise this shitty conf won't work
   os_type = "centos7_64Guest"
-  #calling a template from where we will deploy the vm
+  #call a template
   vapp_template_id = data.vcd_catalog_vapp_template.centos7.id
   # Other instance-specific configurations here...
 
-  #shit network work's for vcd only, cloud-init won't eat this shit
   network {
     adapter_type       = "VMXNET3"
     connected          = true
@@ -54,19 +52,34 @@ resource "vcd_vapp_vm" "TestVm" {
     name               = "s3_net"
     type               = "org"
   }
-  
-  #this shit must be defined otherwise metadata_entry shit won't work
+
+#SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
+  override_template_disk {
+    bus_type = "paravirtual"
+    size_in_mb = "32768"
+    bus_number = 0
+    unit_number = 0
+    #storage_profile = var.vcd_org_ssd_sp
+}
+
+#  disk {
+#    name        = "disk${count.index + 1}"
+#    #label       = "disk${count.index + 1}"
+#    #size        = 4000
+#    unit_number = count.index + 2
+#    bus_number = 2
+#  }
   customization {
     enabled = true
   }
-  #cloud-init https://registry.terraform.io/providers/vmware/vcd/latest/docs/resources/catalog.html#metadata_entry
+  #cloud-shit https://registry.terraform.io/providers/vmware/vcd/latest/docs/resources/catalog.html#metadata_entry
   metadata_entry {
     key   = "user_data"
     value = templatefile("${path.module}/userdata.tpl", {
       ip_address = "192.168.5.${count.index + 236}"
       gateway    = "192.168.5.1"
       hostname  = "test-vm${count.index + 1}"
-      dns_servers = "8.8.8.8"
+      dns_servers = "8.8.4.4"
     })
     is_system = false
     type      = "MetadataStringValue"
@@ -75,7 +88,6 @@ resource "vcd_vapp_vm" "TestVm" {
 }
 
 #Test vm dynamic inventory
-#generating array with ip's
 locals {
   vm_ips = flatten([
     for vm in vcd_vapp_vm.TestVm : [
@@ -84,12 +96,11 @@ locals {
   ])
 }
 
-#outputting the array with ip data
 output "vm_ips" {
   value = local.vm_ips
 }
 
-#filling our template with data from array
+
 resource "local_file" "hosts_cfg" {
   content = templatefile("${path.module}/hosts.tpl",
     {
@@ -99,7 +110,6 @@ resource "local_file" "hosts_cfg" {
   filename = "hosts.cfg"
 }
 
-#ANSIBLE
 resource "null_resource" "ansible_provisioner" {
   count = length(local.vm_ips)
 
@@ -114,4 +124,3 @@ resource "null_resource" "ansible_provisioner" {
     EOT
   }
 }
-
