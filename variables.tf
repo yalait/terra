808d68dfc0d01b3597a522c8521696f744325791
yalait <maksim_smirnovm_95@mail.ru>
2023-08-29 21:37:41 +0700

    fix

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "org_name" {
+  type        = string
+  description = ""
+  default     = "unix_u4"
+}
+
+variable "vm_count" {
+  type        = number
+  description = "description"
+  default     = 3
+}
