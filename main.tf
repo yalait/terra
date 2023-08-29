b2846f7beaa0a53052498f0b8cbc0a8e264049c5
yalait <maksim_smirnovm_95@mail.ru>
2023-08-29 21:34:32 +0700

    рекомендации и предложения)

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 provider "vcd" {
-  user                   = ""
-  password               = ""
-  org                    = ""
-  vdc                    = ""
-  url                    = ""
+  user     = ""
+  password = ""
+  org      = ""
+  vdc      = ""
+  url      = ""
 }
 
 #используется для извлечения информации о каталоге из VMware Cloud Director (vCD).
 data "vcd_catalog" "my-catalog" {
-  org  = "unix_u4"
+  org  = var.org_name
   name = "VK_S3_TEMPL"
 }
 #представляет конфигурацию Terraform для извлечения информации о шаблоне виртуальной машины из каталога VMware Cloud Director (vCD).
 data "vcd_catalog_vapp_template" "centos7" {
-  org        = "unix_u4"
+  org        = var.org_name
   catalog_id = data.vcd_catalog.my-catalog.id
   name       = "vk-temp"
 }
 
 #STORAGE
 resource "vcd_independent_disk" "stor" {
-  count = 3
+  count        = var.vm_count
   name         = "stor${count.index + 1}"
   size_in_mb   = "307200"
   bus_type     = "SCSI"
   bus_sub_type = "VirtualSCSI"
 }
 resource "vcd_independent_disk" "storage" {
-  depends_on = [vcd_independent_disk.stor]
-  count = 3
+  depends_on   = [vcd_independent_disk.stor]
+  count        = var.vm_count
   name         = "storage${count.index + 1}"
   size_in_mb   = "307200"
   bus_type     = "SCSI"
@@ -37,8 +37,8 @@ resource "vcd_independent_disk" "storage" {
 
 #META
 resource "vcd_independent_disk" "meta" {
-  depends_on = [vcd_independent_disk.storage]
-  count = 3
+  depends_on   = [vcd_independent_disk.storage]
+  count        = var.vm_count
   name         = "meta${count.index + 1}"
   size_in_mb   = "153600"
   bus_type     = "SCSI"
@@ -49,14 +49,14 @@ resource "vcd_independent_disk" "meta" {
 #представляет конфигурацию Terraform для создания или управления виртуальной машиной в VMware Cloud Director (vCD).
 resource "vcd_vapp_vm" "storage" {
   depends_on = [vcd_independent_disk.meta]
-  count = 3  # This will create three instances
+  count      = var.vm_count # This will create three instances
 
-  vapp_name     = "vk-storage"
-  name          = "storage-${count.index + 1}"
-  computer_name = "storage-${count.index + 1}"
-  memory        = 6144
-  cpus          = 6
-  cpu_cores     = 1
+  vapp_name        = "vk-storage"
+  name             = "storage-${count.index + 1}"
+  computer_name    = "storage-${count.index + 1}"
+  memory           = 6144
+  cpus             = 6
+  cpu_cores        = 1
   hardware_version = "vmx-15"
 
   os_type = "centos7_64Guest"
@@ -74,28 +74,37 @@ resource "vcd_vapp_vm" "storage" {
     type               = "org"
   }
 
-#SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
-#переопределить дисковые параметры для создания виртуальной машины на основе шаблона виртуальной машины.
+  #SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
+  #переопределить дисковые параметры для создания виртуальной машины на основе шаблона виртуальной машины.
   override_template_disk {
-    bus_type = "paravirtual"
-    size_in_mb = "32768"
-    bus_number = 0
+    bus_type    = "paravirtual"
+    size_in_mb  = "32768"
+    bus_number  = 0
     unit_number = 0
     #storage_profile = var.vcd_org_ssd_sp
-}
+  }
 
-disk {
+  dynamic "disk" {
+    for_each = vcd_independent_disk.stor
+    content {
+      name        = disk.stor[count.index].name
+      bus_number  = 1
+      unit_number = 0
+    }
+  }
+
+  disk {
     name        = vcd_independent_disk.stor[count.index].name
     bus_number  = 1
     unit_number = 0
-}
-disk {
+  }
+  disk {
     name        = vcd_independent_disk.storage[count.index].name
     bus_number  = 1
     unit_number = 1
-}
+  }
 
-#блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
+  #блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
   customization {
     enabled = true
   }
@@ -106,14 +115,14 @@ disk {
 #представляет конфигурацию Terraform для создания или управления виртуальной машиной в VMware Cloud Director (vCD).
 resource "vcd_vapp_vm" "meta" {
   depends_on = [vcd_vapp_vm.storage]
-  count = 3  # This will create three instances
+  count      = 3 # This will create three instances
 
-  vapp_name     = "vk-meta"
-  name          = "meta-${count.index + 1}"
-  computer_name = "meta-${count.index + 1}"
-  memory        = 18432
-  cpus          = 10
-  cpu_cores     = 1
+  vapp_name        = "vk-meta"
+  name             = "meta-${count.index + 1}"
+  computer_name    = "meta-${count.index + 1}"
+  memory           = 18432
+  cpus             = 10
+  cpu_cores        = 1
   hardware_version = "vmx-15"
 
   os_type = "centos7_64Guest"
@@ -131,23 +140,23 @@ resource "vcd_vapp_vm" "meta" {
     type               = "org"
   }
 
-#SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
-#переопределить дисковые параметры для создания виртуальной машины на основе шаблона виртуальной машины.
+  #SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
+  #переопределить дисковые параметры для создания виртуальной машины на основе шаблона виртуальной машины.
   override_template_disk {
-    bus_type = "paravirtual"
-    size_in_mb = "32768"
-    bus_number = 0
+    bus_type    = "paravirtual"
+    size_in_mb  = "32768"
+    bus_number  = 0
     unit_number = 0
     #storage_profile = var.vcd_org_ssd_sp
-}
+  }
 
-disk {
+  disk {
     name        = vcd_independent_disk.meta[count.index].name
     bus_number  = 1
     unit_number = 0
-}
+  }
 
-#блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
+  #блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
   customization {
     enabled = true
   }
@@ -157,14 +166,14 @@ disk {
 #представляет конфигурацию Terraform для создания или управления виртуальной машиной в VMware Cloud Director (vCD).
 resource "vcd_vapp_vm" "front" {
   depends_on = [vcd_vapp_vm.meta]
-  count = 2  # This will create three instances
+  count      = 2 # This will create three instances
 
-  vapp_name     = "vk-front"
-  name          = "front-${count.index + 1}"
-  computer_name = "front-${count.index + 1}"
-  memory        = 16384
-  cpus          = 8
-  cpu_cores     = 1
+  vapp_name        = "vk-front"
+  name             = "front-${count.index + 1}"
+  computer_name    = "front-${count.index + 1}"
+  memory           = 16384
+  cpus             = 8
+  cpu_cores        = 1
   hardware_version = "vmx-15"
 
   os_type = "centos7_64Guest"
@@ -182,17 +191,17 @@ resource "vcd_vapp_vm" "front" {
     type               = "org"
   }
 
-#SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
-#переопределить дисковые параметры для создания виртуальной машины на основе шаблона виртуальной машины.
+  #SHIT FUNCTION not mentioned in https://registry.terraform.io/providers/vmware/vcd/latest/docs this bull shit override your template disk while creating vm
+  #переопределить дисковые параметры для создания виртуальной машины на основе шаблона виртуальной машины.
   override_template_disk {
-    bus_type = "paravirtual"
-    size_in_mb = "32768"
-    bus_number = 0
+    bus_type    = "paravirtual"
+    size_in_mb  = "32768"
+    bus_number  = 0
     unit_number = 0
     #storage_profile = var.vcd_org_ssd_sp
-}
+  }
 
-#блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
+  #блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
   customization {
     enabled = true
   }
@@ -201,13 +210,13 @@ resource "vcd_vapp_vm" "front" {
 #MONITOR
 #представляет конфигурацию Terraform для создания или управления виртуальной машиной в VMware Cloud Director (vCD).
 resource "vcd_vapp_vm" "monitoring" {
-  depends_on = [vcd_vapp_vm.front]
-  vapp_name     = "vk-monitoring"
-  name          = "monitor-1"
-  computer_name = "monitor-1"
-  memory        = 12288
-  cpus          = 6
-  cpu_cores     = 1
+  depends_on       = [vcd_vapp_vm.front]
+  vapp_name        = "vk-monitoring"
+  name             = "monitor-1"
+  computer_name    = "monitor-1"
+  memory           = 12288
+  cpus             = 6
+  cpu_cores        = 1
   hardware_version = "vmx-15"
 
   os_type = "centos7_64Guest"
@@ -225,7 +234,7 @@ resource "vcd_vapp_vm" "monitoring" {
     type               = "org"
   }
 
-#блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
+  #блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
   customization {
     enabled = true
   }
@@ -234,13 +243,13 @@ resource "vcd_vapp_vm" "monitoring" {
 #BALANCER
 #представляет конфигурацию Terraform для создания или управления виртуальной машиной в VMware Cloud Director (vCD).
 resource "vcd_vapp_vm" "balancer" {
-  depends_on = [vcd_vapp_vm.monitoring]
-  vapp_name     = "vk-balancer"
-  name          = "balancer-1"
-  computer_name = "balancer-1"
-  memory        = 6144
-  cpus          = 4
-  cpu_cores     = 1
+  depends_on       = [vcd_vapp_vm.monitoring]
+  vapp_name        = "vk-balancer"
+  name             = "balancer-1"
+  computer_name    = "balancer-1"
+  memory           = 6144
+  cpus             = 4
+  cpu_cores        = 1
   hardware_version = "vmx-15"
 
   os_type = "centos7_64Guest"
@@ -258,7 +267,7 @@ resource "vcd_vapp_vm" "balancer" {
     type               = "org"
   }
 
-#блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
+  #блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
   customization {
     enabled = true
   }
@@ -267,13 +276,13 @@ resource "vcd_vapp_vm" "balancer" {
 #IAM
 #представляет конфигурацию Terraform для создания или управления виртуальной машиной в VMware Cloud Director (vCD).
 resource "vcd_vapp_vm" "iam" {
-  depends_on = [vcd_vapp_vm.balancer]
-  vapp_name     = "S3"
-  name          = "iam"
-  computer_name = "iam"
-  memory        = 4096
-  cpus          = 4
-  cpu_cores     = 1
+  depends_on       = [vcd_vapp_vm.balancer]
+  vapp_name        = "S3"
+  name             = "iam"
+  computer_name    = "iam"
+  memory           = 4096
+  cpus             = 4
+  cpu_cores        = 1
   hardware_version = "vmx-15"
 
   os_type = "centos7_64Guest"
@@ -291,7 +300,7 @@ resource "vcd_vapp_vm" "iam" {
     type               = "org"
   }
 
-#блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
+  #блок используется для определения параметров, которые позволяют настроить виртуальную машину на этапе развертывания.
   customization {
     enabled = true
   }
@@ -301,66 +310,66 @@ resource "vcd_vapp_vm" "iam" {
 #Фрагмент кода, который вы предоставили, использует блок locals в Terraform для создания переменной, которая будет содержать список IP-адресов виртуальных машин,
 # определенных в ресурсе vcd_vapp_vm.xxx
 locals {
-    #STOR
-    vm_ips_storage = flatten([
+  #STOR
+  vm_ips_storage = flatten([
     for vm in vcd_vapp_vm.storage : [
       for network in vm.network : network.ip
     ]
-    ])
-    vm_names_storage = [
+  ])
+  vm_names_storage = [
     for vm_instance in vcd_vapp_vm.storage : vm_instance.computer_name
-    ]
-    #---
-    #META
-    vm_ips_meta = flatten([
+  ]
+  #---
+  #META
+  vm_ips_meta = flatten([
     for vm in vcd_vapp_vm.meta : [
       for network in vm.network : network.ip
     ]
-    ])
-    vm_names_meta = [
+  ])
+  vm_names_meta = [
     for vm_instance in vcd_vapp_vm.meta : vm_instance.computer_name
-    ]
-    #---
-    #FRONT
-    vm_ips_front = flatten([
+  ]
+  #---
+  #FRONT
+  vm_ips_front = flatten([
     for vm in vcd_vapp_vm.front : [
       for network in vm.network : network.ip
     ]
-    ])
-    vm_names_front = [
+  ])
+  vm_names_front = [
     for vm_instance in vcd_vapp_vm.front : vm_instance.computer_name
-    ]
-    #---
-    #BALANCER
-    vm_ips_balancer = vcd_vapp_vm.balancer.network[0].ip
-    vm_names_balancer = vcd_vapp_vm.balancer.computer_name
-    #---
-    #MONITORING
-    vm_ips_monitoring = vcd_vapp_vm.monitoring.network[0].ip
-    vm_names_monitoring = vcd_vapp_vm.monitoring.computer_name
-    #---
-    #IAM
-    vm_ips_iam = vcd_vapp_vm.iam.network[0].ip
-    vm_names_iam = vcd_vapp_vm.iam.computer_name
+  ]
+  #---
+  #BALANCER
+  vm_ips_balancer   = vcd_vapp_vm.balancer.network[0].ip
+  vm_names_balancer = vcd_vapp_vm.balancer.computer_name
+  #---
+  #MONITORING
+  vm_ips_monitoring   = vcd_vapp_vm.monitoring.network[0].ip
+  vm_names_monitoring = vcd_vapp_vm.monitoring.computer_name
+  #---
+  #IAM
+  vm_ips_iam   = vcd_vapp_vm.iam.network[0].ip
+  vm_names_iam = vcd_vapp_vm.iam.computer_name
 
-    #ZIPMAP for key value
-    storage = zipmap(local.vm_names_storage, local.vm_ips_storage)
-    meta = zipmap(local.vm_names_meta, local.vm_ips_meta)
-    front = zipmap(local.vm_names_front, local.vm_ips_front)
-    #тут изначально получили строки, так как вм создаются в единичном экземпляре,поэтому насильно заводим в массив
-    balancer = zipmap([local.vm_names_balancer], [local.vm_ips_balancer])
-    monitoring = zipmap([local.vm_names_monitoring], [local.vm_ips_monitoring])
-    iam = zipmap([local.vm_names_iam], [local.vm_ips_iam])
-    #Создаём 1 общий объект
-    combined_data = merge(
+  #ZIPMAP for key value
+  storage = zipmap(local.vm_names_storage, local.vm_ips_storage)
+  meta    = zipmap(local.vm_names_meta, local.vm_ips_meta)
+  front   = zipmap(local.vm_names_front, local.vm_ips_front)
+  #тут изначально получили строки, так как вм создаются в единичном экземпляре,поэтому насильно заводим в массив
+  balancer   = zipmap([local.vm_names_balancer], [local.vm_ips_balancer])
+  monitoring = zipmap([local.vm_names_monitoring], [local.vm_ips_monitoring])
+  iam        = zipmap([local.vm_names_iam], [local.vm_ips_iam])
+  #Создаём 1 общий объект
+  combined_data = merge(
     local.storage,
     local.meta,
     local.front,
     local.balancer,
     local.monitoring,
     local.iam
-    )
-    #combined_array = concat(local.storage, local.meta, local.front, local.balancer, local.monitoring, local.iam)
+  )
+  #combined_array = concat(local.storage, local.meta, local.front, local.balancer, local.monitoring, local.iam)
 }
 
 #в Terraform используется для создания вывода, который предоставляет доступ к определенным значениям или переменным из вашей конфигурации Terraform после её выполнения. В данном случае, output "vm_ips" позволяет вам создать вывод с именем vm_ips и присвоить ему значение, которое содержится в переменной local.vm_ips.
@@ -378,12 +387,12 @@ output "vm_ips_storage" {
 resource "local_file" "hosts_cfg" {
   content = templatefile("${path.module}/inv.tpl",
     {
-      vm_ips_storage = local.vm_ips_storage
-      vm_ips_meta = local.vm_ips_meta
-      vm_ips_front = local.vm_ips_front
-      vm_ips_monitoring= local.vm_ips_monitoring
-      vm_ips_balancer= local.vm_ips_balancer
-      vm_ips_iam= local.vm_ips_iam
+      vm_ips_storage    = local.vm_ips_storage
+      vm_ips_meta       = local.vm_ips_meta
+      vm_ips_front      = local.vm_ips_front
+      vm_ips_monitoring = local.vm_ips_monitoring
+      vm_ips_balancer   = local.vm_ips_balancer
+      vm_ips_iam        = local.vm_ips_iam
     }
   )
   filename = "inv.cfg"
@@ -393,7 +402,7 @@ resource "local_file" "hosts_cfg" {
 resource "local_file" "inv_cfg" {
   content = templatefile("${path.module}/hosts.tpl",
     {
-    #это всё массивы данных с key-value
+      #это всё массивы данных с key-value
       array = local.combined_data
     }
   )
